05175ae6713d22d8a21da621d73312945ef33066
Zubair <zubair99ss@hotmail.com>
2023-10-29 22:15:07 +0800

    Complete CLI setup and provide entry point

--- DirectoryTree/cli.py
+++ DirectoryTree/cli.py
@@ -3,3 +3,32 @@
 import argparse
 import pathlib
 import sys
+ 
+from . import __version__
+from .Directory_Tree import DirectoryTree
+
+def main():
+    args = parse_cmd_line_arguments()
+    root_dir = pathlib.Path(args.rooot_dir)
+    if not root_dir.is_dir():
+        print("The specified root directory doesn't exist.")
+        sys.exit
+    tree = DirectoryTree(root_dir)
+    tree.generate()
+
+def parse_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        prog = "tree",
+        description = "Directory Tree, a directory tree generator",
+        epilog = "Thanks for using Directory Tree!"
+    )
+    parser.version = f"Directory TRee v{__version__}"
+    parser.add_argument("-v", "--version", action = "version")
+    parser.add_argument(
+        "root_dir",
+        metavar = "ROOT_DIR",
+        nargs = "?",
+        default = ".",
+        help = "Generate a full directory tree starting at ROOT_DIR",
+    )
+    return parser.parse_args()
---
+++ tree.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# tree.py
+
+"""This module provides Directory Tree entry point script."""
+
+from DirectoryTree.cli import main
+
+if __name__ == "__main__":
+    main()
